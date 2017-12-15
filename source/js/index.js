16acd1a30df43c43b3fa2c824009a9ebdca6e3b8
Becky Rush <becky.rush14@yahoo.co.uk>
2017-12-15 18:29:18 +0000

    pull in data from google sheets and test making graph

--- source/js/index.js
+++ source/js/index.js
@@ -1,21 +1,56 @@
 import * as d3 from "d3";
+import $ from 'jquery';
 
-var w = 300;
-var h = 100;
-var padding = 2;
-var dataset = [5, 10, 14, 20, 25];
-
-var svg = d3.select("body").append("svg")
-            .attr('width', w)
-            .attr('height', h);
-svg.selectAll('rect')
-  .data(dataset)
-  .enter()
-  .append('rect')
-.attr({
-  x: function(d, index){return index * (w / dataset.length);}),
-  y: function(d){ return h - (d*4); }),
-  width: w / dataset.length - padding,
-  height: funciton(d) { return d*10; },
-  fill: function(d) { return `rgb(${d*10}, 0, 0)`; }
+let dataset = "http://gsx2json.com/api?id=1KfV5BohkeZMAjMWHBua23O02rP6EGCMDNUgySNuMhQo&integers=FALSE"
+$.getJSON(dataset).done(dataset=> {
+    start(dataset);
 });
+
+function start(dataset) {
+  let eminemData = [];
+  dataset.rows.forEach((track) => {
+    let artist = track.artist.toString();
+    if (artist.includes("EMINEM")) {
+      eminemData.push(track);
+    }
+  })
+  createGraph(eminemData);
+}
+
+function createGraph(eminemData) {
+  console.log(eminemData);
+  var w = 300;
+  var h = 100;
+  var padding = 2;
+  var graphData = [];
+  eminemData.forEach((track) => {
+    graphData.push(parseFloat(track.danceability));
+  });
+  console.log(graphData);
+
+  var svg = d3.select(".graph").append("svg")
+              .attr('width', w)
+              .attr('height', h);
+  svg.selectAll('rect')
+    .data(graphData)
+    .enter()
+      .append('rect')
+      .attr("x", function(d, index) {return index * (w / graphData.length);})
+      .attr("y", function(d) { return h - (d*40);})
+      .attr("width", (w / graphData.length - padding))
+      .attr("height", function(d) {return (d*100);})
+      .attr("fill", function(d) {return `rgb(${parseInt(d*50)}, ${parseInt(d*150)}, 0)`;});
+
+  var text = svg.selectAll("text")
+                          .data(eminemData)
+                          .enter()
+                          .append("text")
+  var textLabels = text
+                    .attr("x", function(d, index) {return index * (w / eminemData.length);})
+                    .attr("y", 100)
+                    .text(function (d){return d.title})
+                    .attr("font-family", "sans-serif")
+                    .attr("font-size", "20px")
+                    .attr("fill", "red");
+                    // .attr("transform", "rotate(180)");
+}
