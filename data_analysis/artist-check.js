69fc782a3870c162121d6f817b168c9575c3f678
Becky Rush <becky.rush14@yahoo.co.uk>
2018-01-22 19:18:24 +0000

    unique artist count updated for spelling corrections

--- data_analysis/artist-check.js
+++ data_analysis/artist-check.js
@@ -1,8 +1,8 @@
 var writeJson = require('write-json');
-var uniqueArtists = require('./uniqueArtists.json');
-var all_tracks = require('./fixed_data_for_analysis.json');
-var outputFile = './unique_artists_track_count.json';
-let output = {};
+var uniqueArtists = require('./data/uniqueArtists.json');
+var all_tracks = require('../data_gathering/redo-data-gathering/compiledData.json');
+var outputFile = './data/unique_artists_track_count.json';
+let output = [];
 let id = 0;
 
 uniqueArtists.forEach((artist) => {
@@ -11,9 +11,9 @@ uniqueArtists.forEach((artist) => {
     currentArtist = String(currentArtist);
   }
   let count = 0;
-  output[id] = {};
-  output[id]["artist"] = currentArtist;
-  output[id]['total_weeks'] = 0;
+  trackDetails = {};
+  trackDetails["artist"] = currentArtist;
+  trackDetails['total_weeks'] = 0;
   all_tracks.forEach((track) => {
     var trackArtist = track.artist;
     if (typeof trackArtist !== "string") {
@@ -21,16 +21,28 @@ uniqueArtists.forEach((artist) => {
     }
     if (trackArtist.search(`\\b${currentArtist}\\b`) != -1) {
       count++;
-      output[id]['count'] = count;
-      output[id]['total_weeks'] += parseInt(track.weeks_at_1);
+      trackDetails['count'] = count;
+      trackDetails['total_weeks'] += parseInt(track.weeks_at_1);
     }
   });
+  output.push(trackDetails);
+  writeOutOutput();
   id++;
 });
-writeJson(outputFile, output, function(err) {
-  if (err) {
-    console.log('error :(  : ', err);
-  } else {
-    console.log('done! :)');
-  }
-})
+
+function writeOutOutput() {
+  writeJson(outputFile, output, function(err) {
+    if (err) {
+      console.log('oops, error writing to file ', err);
+    } else {
+      console.log('data file made');
+    }
+  })
+}
+
+// var uniqueArtistsFailed = require('./data/unique_artists_track_count.json');
+// uniqueArtistsFailed.forEach((artist) => {
+//   if (!artist.count) {
+//     console.log(artist.artist);
+//   }
+// });
