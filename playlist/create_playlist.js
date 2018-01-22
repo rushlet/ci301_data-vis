e72523eb454f22a1f75abc080527e6926b8c9519
Becky Rush <becky.rush14@yahoo.co.uk>
2018-01-22 17:40:36 +0000

    redo data gathering after a lot of incorrect data identified

--- playlist/create_playlist.js
+++ playlist/create_playlist.js
@@ -1,13 +1,17 @@
 var SpotifyWebApi = require('spotify-web-api-node');
 var writeJson = require('write-json');
 var keys = require('../data_gathering/spotify_keys.json')
-var tracks = require('../data_analysis/data/fixed_data_for_analysis.json')
+var tracks = require('../data_gathering/redo-data-gathering/compiledData.json')
 var client = keys["2"]["client"];
 var secret = keys["2"]["secret"];
 var outputFile = './failed.json';
-let failed = {};
+let output = {};
+let failed = [];
 let itemsProcessed = 0;
 let id = 0;
+
+let failedTracks = require('./failed.json')
+console.log(failedTracks.length);
 
 var spotifyApi = new SpotifyWebApi({
   clientId : client,
@@ -15,23 +19,16 @@ var spotifyApi = new SpotifyWebApi({
   redirectUri : 'http://localhost:8888/callback'
 });
 
-spotifyApi.setAccessToken('BQD8IfkP74KMHFJ2ZkmIXtEbUnDJtEmFid63P53-E6qIPbN6C7IIV7ATVEVRnDeCF7ml_iQAussPyI7ckXWgdRyybApHtXUF4_Yh3q8dpQiYemWRC9TGLVBn6X3jgx2Lk3dKLZHXlmhr-M09rWnJaQUd39bL9DfAApQNFFaP6JfX1WS38yKCenleeUU&refresh_token=AQB6gtcSMEStESuIwOaDJxwDHXtEsCZWBSCmmS87qVyWk5gPDAbldqiA1MaBGQKcCIs6y-plq2iiF3ptxp1ITqTz5PyhF8_W5M9J9jMewi2RGCuO_DyEHpUnlDUzyF6LMvA');
+spotifyApi.setAccessToken('BQD3gKWwZgdEdJ7dyClKmrQMyPJ8BGC8fxQIpgMj3IA8VUv2bYDYmyIgfa3v80ZH-SipSmprygkgcHCNU-g9L9UiF57de5TixuFQ1Fnuy7UV_nv0UdBtqAhk6cDPf4dZOBb39526Sn9YJV_doXmGEktxMHn9ihlBppRkUmZqimnult0X_-_KIC0Z72A');
 
-tracks.forEach((track)=>{
-  let spotifyID = track.spotify_id;
-  spotifyApi.addTracksToPlaylist('rushlet', '6DNZV1L405XpElhIAUHaKZ', [`spotify:track:${spotifyID}`], {}, ((err, data) => {
-    setTimeout(function () {
-      if (err) {
-        console.log('Something went wrong!', track.title, ' : ' , err);
-        failed[id] = `spotify:track:${spotifyID}`;
-        id++;
-      } else {
-        console.log(`Added ${track.title} to playlist!`);
-      }
-    }, 2000);
-    itemsProcessed++;
-    if(itemsProcessed === tracks.length) {
+failedTracks.forEach((track)=>{
+  spotifyApi.addTracksToPlaylist('rushlet', '6DNZV1L405XpElhIAUHaKZ', [`spotify:track:${track.spotify_id}`], {}, ((err, data) => {
+    if (err) {
+      console.log('Something went wrong!', track.title, ' : ' , err);
+      failed.push(track);
       writeOutFailures();
+    } else {
+      console.log(`Added ${track} to playlist!`);
     }
   })
   );
