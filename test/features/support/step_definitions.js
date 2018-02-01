f4f5c1c8253e75b9a832b44f1cf24f46dbf03e64
Becky Rush <becky.rush14@yahoo.co.uk>
2018-02-01 18:02:39 +0000

    start setting up cucumber tests

---
+++ test/features/support/step_definitions.js
@@ -0,0 +1,16 @@
+var cucumber = require('cucumber');
+cucumber.When('I first land on the page', function (callback) {
+ // Write code here that turns the phrase above into concrete actions
+ callback(null, 'pending');
+ return true;
+});
+
+cucumber.Then('I should see only the first section, with the title', function (callback) {
+ // Write code here that turns the phrase above into concrete actions
+ callback(null, 'pending');
+});
+
+cucumber.Then('scroll instruction', function (callback) {
+ // Write code here that turns the phrase above into concrete actions
+ callback(null, 'pending');
+});
