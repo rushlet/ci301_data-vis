55d457074254f1ef0633dd48374a30b71ba4fe40
Becky Rush <becky.rush14@yahoo.co.uk>
2018-02-02 09:59:34 +0000

    update test and add new scenario

--- test/features/support/step_definitions.js
+++ test/features/support/step_definitions.js
@@ -1,4 +1,5 @@
 var cucumber = require('cucumber');
+
 cucumber.When('I first land on the page', function (callback) {
  // Write code here that turns the phrase above into concrete actions
  callback(null, 'pending');
@@ -10,7 +11,27 @@ cucumber.Then('I should see only the first section, with the title', function (c
  callback(null, 'pending');
 });
 
-cucumber.Then('scroll instruction', function (callback) {
+cucumber.Then('I should see the scroll instruction', function (callback) {
  // Write code here that turns the phrase above into concrete actions
  callback(null, 'pending');
 });
+
+cucumber.When('I scroll down to the first section', function (callback) {
+   // Write code here that turns the phrase above into concrete actions
+   callback(null, 'pending');
+ });
+
+cucumber.Then('I should see the swarm graph', function (callback) {
+   // Write code here that turns the phrase above into concrete actions
+   callback(null, 'pending');
+ });
+
+ cucumber.Then('as I scroll further, the graph should stay static', function (callback) {
+   // Write code here that turns the phrase above into concrete actions
+   callback(null, 'pending');
+ });
+
+ cucumber.Then('the text should scroll', function (callback) {
+   // Write code here that turns the phrase above into concrete actions
+   callback(null, 'pending');
+ });
--- test/features/uk_number_1s.feature
+++ test/features/uk_number_1s.feature
@@ -3,4 +3,10 @@ Feature: A Visual Analysis of UK Number 1s
 Scenario: Default view
 	When I first land on the page
 	Then I should see only the first section, with the title
-	And scroll instruction
+	And I should see the scroll instruction
+
+Scenario: First section
+	When I scroll down to the first section
+	Then I should see the swarm graph
+	And as I scroll further, the graph should stay static
+	But the text should scroll
