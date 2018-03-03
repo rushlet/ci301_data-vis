9d2f5a217386e5c31e37927e719f066e76794829
Becky Rush <becky.rush14@yahoo.co.uk>
2018-03-03 15:23:04 +0000

    capitalise first leter after brace

--- source/js/data-cleaner.js
+++ source/js/data-cleaner.js
@@ -54,7 +54,15 @@ export function meanData() {
 }
 
 export function capitalize(string) {
-    return string.toLowerCase().replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
+  var splitStr = string.toLowerCase().split(' ');
+  for (var i = 0; i < splitStr.length; i++) {
+    if (!splitStr[i].charAt(0).match(/[a-z]/i) && splitStr[i].charAt(1).match(/[a-z]/i)) {
+      splitStr[i] = splitStr[i].charAt(0) + splitStr[i].charAt(1).toUpperCase() + splitStr[i].substring(2);
+    } else {
+      splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+    }
+  }
+  return splitStr.join(' ');
 };
 
 export function cleanDataForBarChart() {
