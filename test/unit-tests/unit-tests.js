1a0acd4f379fd94985c65c41ffba169fbf1f8b7c
Becky Rush <becky.rush14@yahoo.co.uk>
2018-03-07 17:55:08 +0000

    capitalize unit tests (passing)

--- test/unit-tests/unit-tests.js
+++ test/unit-tests/unit-tests.js
@@ -4,9 +4,22 @@ var assert = chai.assert;
 
 describe('Capitalize', function() {
   it('should capitalize the first letter of each word in a string', function() {
-    var testString1 = "hello, my name is becky";
-    var capitalisedString1 = functions.capitalize(testString1);
-    var expectString1 = "Hello, My Name Is Becky";
-    assert.equal(capitalisedString1, expectString1);
+    var testString = "hello, my name is becky";
+    var capitalisedString = functions.capitalize(testString);
+    var expectString = "Hello, My Name Is Becky";
+    assert.equal(capitalisedString, expectString);
   });
+  it('should capitalize the first letter of each word in a string, even if it starts with a bracket', function() {
+    var testString = "(hello) my (name) is becky";
+    var capitalisedString = functions.capitalize(testString);
+    var expectString = "(Hello) My (Name) Is Becky";
+    assert.equal(capitalisedString, expectString);
+  });
+  it('should capitalize the first letter of each word in a string, and the other letters should be lowercase', function() {
+    var testString = "hELLo, my NAme Is bECKy";
+    var capitalisedString = functions.capitalize(testString);
+    var expectString = "Hello, My Name Is Becky";
+    assert.equal(capitalisedString, expectString);
+  });
+
 });
