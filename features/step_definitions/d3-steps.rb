e71a28ef6498f20c21537b64d4549564fea158e8
Becky Rush <becky.rush14@yahoo.co.uk>
2018-03-09 19:08:10 +0000

    fix false positive test

--- features/step_definitions/d3-steps.rb
+++ features/step_definitions/d3-steps.rb
@@ -17,20 +17,16 @@ end
 When(/^I scroll down to the artist section$/) do
   scroll_to(page.find(".section-artists", visible: true))
   page.save_screenshot 'features/reports/screenshots/screenshot-test-scroll.png'
-  page.execute_script "window.close();"
 end
 
 Then(/^I should see a swarm chart$/) do
   scroll_to(page.find("#swarm-chart", visible: true))
   page.save_screenshot 'features/reports/screenshots/swarm-chart.png'
-  page.execute_script "window.close();"
 end
 
 Given(/^I am on the swarm chart$/) do
-  within_webpage do
-    scroll_to(page.find("#swarm-chart", visible: true))
-  end
-  page.execute_script "window.close();"
+  step 'I am on the project page'
+  scroll_to(page.find("#swarm-chart", visible: true))
 end
 
 When(/^I scroll to the 'longest' section$/) do
@@ -39,11 +35,9 @@ When(/^I scroll to the 'longest' section$/) do
     scroll_to(page.find('div[data-step="swarm--longest"]', visible: true))
     page.save_screenshot 'features/reports/screenshots/swarm-chart--longest-scroll.png'
   end
-  page.execute_script "window.close();"
 end
 
 Then(/^I should see the chart zoom in and pan to the right$/) do
-
   # transform should be applied to svg, translate
   # transform="translate(-1950, 150) scale(5.5,5.5)"
   pending # Write code here that turns the phrase above into concrete actions
--- features/step_definitions/helpers.rb
+++
@@ -1,5 +0,0 @@
-def within_webpage
-  visit 'https://rushlet.github.io/ci301_data-vis/website/project.html' do
-    yield
-  end
-end
