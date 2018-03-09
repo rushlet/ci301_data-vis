7e010ae7238d397cb4c3565fdb84daa13a5a2170
Becky Rush <becky.rush14@yahoo.co.uk>
2018-03-09 11:03:22 +0000

    fix syntax deprecation

--- features/step_definitions/index-steps.rb
+++ features/step_definitions/index-steps.rb
@@ -3,9 +3,10 @@ When(/^I land on the index page$/) do
 end
 
 Then(/^I should see a prompt to log in with Spotify$/) do
-  page.expect have_css('h2', :text => 'Log in with Spotify')
+  expect(page).to have_css('h2', :text => 'Log in with Spotify')
 end
 
 Then(/^I should see two buttons \- log in and skip$/) do
-
+  expect(page).to have_css('button', :text => 'Log into Spotify')
+  expect(page).to have_css('button', :text => 'I don\'t have Spotify')
 end
