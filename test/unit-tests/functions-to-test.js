b5bea737ac6332ad71938bd1d652c075223d2ef5
Becky Rush <becky.rush14@yahoo.co.uk>
2018-03-14 12:43:53 +0000

    add unit test for barChartDataFormat (currently failing)

--- test/unit-tests/functions-to-test.js
+++ test/unit-tests/functions-to-test.js
@@ -9,5 +9,112 @@ module.exports = {
       }
     }
     return splitStr.join(' ');
+  },
+
+  cleanDataForBarChart: function(song1, song2) {
+    return [{
+            'title': song1.title,
+            'artist': song1.artist,
+            'features': {
+              'danceability': song1.danceability,
+              'valence': song1.valence,
+              'acousticness': song1.acousticness,
+              'energy': song1.energy,
+              'instrumentalness': song1.valence,
+              'liveness': song1.acousticness,
+              'speechiness': song1.energy,
+            }
+          },
+          {
+            'title': song2.title,
+            'artist': song2.artist,
+            'features': {
+              'danceability': song2.danceability,
+              'valence': song2.valence,
+              'acousticness': song2.acousticness,
+              'energy': song2.energy,
+              'instrumentalness': song2.valence,
+              'liveness': song2.acousticness,
+              'speechiness': song2.energy,
+            }
+          }];
   }
+
+  // overallAverages: function(data) {
+  //   let count = 0;
+  //   let audioFeatures = ['danceability', 'energy', 'valence', 'acousticness', 'speechiness', 'liveness', 'instrumentalness', 'duration_ms'];
+  //   let yearlyAverages = [];
+  //   data.forEach((d) => {
+  //     if (d.danceability) {
+  //       count ++;
+  //       audioFeatures.forEach((feature) => {
+  //         if (averages[feature]) {
+  //           averages[feature] += d[feature];
+  //         } else {
+  //           averages[feature] = d[feature];
+  //         }
+  //       });
+  //     }
+  //   });
+  //   for (var feature in averages) {
+  //     averages[feature] = averages[feature] / count;
+  //   }
+  // }
+  //
+  // yearlyAverages: function(data) {
+  // }
+
+}
+
+function meanData() {
+  let data = config['dataset'];
+  let averages = {};
+  let years = new Set();
+  let count = 0;
+  let audioFeatures = ['danceability', 'energy', 'valence', 'acousticness', 'speechiness', 'liveness', 'instrumentalness', 'duration_ms'];
+  let yearlyAverages = [];
+  data.forEach((d) => {
+    years.add(d.year);
+    if (d.danceability) {
+      count ++;
+      audioFeatures.forEach((feature) => {
+        if (averages[feature]) {
+          averages[feature] += d[feature];
+        } else {
+          averages[feature] = d[feature];
+        }
+      });
+    }
+  });
+  for (var feature in averages) {
+    averages[feature] = averages[feature] / count;
+  }
+
+  years.forEach((year) => {
+    let counter = 0;
+    let output = {};
+    data.forEach((d) => {
+      if (d.year === year) {
+        output['year'] = year;
+        counter++;
+        if (d.danceability) {
+          output['count'] = counter;
+          audioFeatures.forEach((feature) => {
+            if (output[feature]) {
+              output[feature] += d[feature];
+            } else {
+              output[feature] = d[feature];
+            }
+          });
+        }
+      }
+    });
+    audioFeatures.forEach((feature) => {
+        output[feature] = (output[feature] / output['count']).toFixed(3);
+    });
+    yearlyAverages.push(output);
+  })
+
+  config["yearlyAverages"] = yearlyAverages;
+  config["overallAverages"] = averages;
 }
--- test/unit-tests/unit-tests.js
+++ test/unit-tests/unit-tests.js
@@ -2,6 +2,55 @@ var chai = require('chai');
 var functions = require('./functions-to-test.js');
 var assert = chai.assert;
 
+var song1 = {
+    "title": "HELP!",
+    "artist": "THE BEATLES",
+    "spotify_id": "1dfuJYDSIc41cw5RPsaCF1",
+    "danceability": 0.602,
+    "energy": 0.736,
+    "key": 11,
+    "loudness": -6.51,
+    "speechiness": 0.0332,
+    "acousticness": 0.149,
+    "instrumentalness": 0,
+    "liveness": 0.0776,
+    "valence": 0.819,
+    "tempo": 94.931,
+    "duration_ms": 139240,
+    "time_signature": 4,
+    "date": "05/08/1965",
+    "decade": 1960,
+    "weeks_at_1": 3,
+    "consecutive": "TRUE",
+    "times_at_one": 1,
+    "year": 1965,
+    "preview_url": "https://p.scdn.co/mp3-preview/46aeed8650533caf77314fe7e46a015516bcaa1a?cid=ddba468408e2427090e0d79450f3d535"
+  };
+var song2 =   {
+    "title": "LET'S DANCE",
+    "artist": "DAVID BOWIE",
+    "spotify_id": "0F0MA0ns8oXwGw66B2BSXm",
+    "danceability": 0.619,
+    "energy": 0.703,
+    "key": 10,
+    "loudness": -9.414,
+    "speechiness": 0.0525,
+    "acousticness": 0.00388,
+    "instrumentalness": 0.205,
+    "liveness": 0.163,
+    "valence": 0.709,
+    "tempo": 115.043,
+    "duration_ms": 457133,
+    "time_signature": 4,
+    "date": "09/04/1983",
+    "decade": 1980,
+    "weeks_at_1": 3,
+    "consecutive": "TRUE",
+    "times_at_one": 1,
+    "year": 1983,
+    "preview_url": "https://p.scdn.co/mp3-preview/1157e120445585f0155b9d1ca303029b8068d9a4?cid=ddba468408e2427090e0d79450f3d535"
+  };
+
 describe('Capitalize', function() {
   it('should capitalize the first letter of each word in a string', function() {
     var testString = "hello, my name is becky";
@@ -21,5 +70,36 @@ describe('Capitalize', function() {
     var expectString = "Hello, My Name Is Becky";
     assert.equal(capitalisedString, expectString);
   });
+});
 
+describe('cleanDataForBarChart', function() {
+  it('should format data from the charts dataset into an object to be consumed by the bar chart', function() {
+      var output = [{
+              'title': "HELP!",
+              'artist': "THE BEATLES",
+              'features': {
+                'danceability': 0.602,
+                'valence': 0.819,
+                'acousticness': 0.149,
+                'energy': 0.736,
+                'instrumentalness': 0,
+                'liveness': 0.163,
+                'speechiness': 0.0332,
+              }
+            },
+            {
+              'title': "LET'S DANCE",
+              'artist': "DAVID BOWIE",
+              'features': {
+                'danceability': 0.619,
+                'valence': 0.709,
+                'acousticness': 0.00388,
+                'energy': 0.703,
+                'instrumentalness': 0.205,
+                'liveness': 0.163,
+                'speechiness': 0.0525,
+              }
+      }];
+      assert.equal(functions.cleanDataForBarChart(song1, song2), output);
+  });
 });
