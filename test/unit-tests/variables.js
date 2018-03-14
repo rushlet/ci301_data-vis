037df1cef9c0257fc07c4231fc0434380f27a36e
Becky Rush <becky.rush14@yahoo.co.uk>
2018-03-14 13:29:45 +0000

    extract variables into separate file

---
+++ test/unit-tests/variables.js
@@ -0,0 +1,79 @@
+var chartsSong1 = {
+    "title": "HELP!",
+    "artist": "THE BEATLES",
+    "spotify_id": "1dfuJYDSIc41cw5RPsaCF1",
+    "danceability": 0.602,
+    "energy": 0.736,
+    "key": 11,
+    "loudness": -6.51,
+    "speechiness": 0.0332,
+    "acousticness": 0.149,
+    "instrumentalness": 0,
+    "liveness": 0.0776,
+    "valence": 0.819,
+    "tempo": 94.931,
+    "duration_ms": 139240,
+    "time_signature": 4,
+    "date": "05/08/1965",
+    "decade": 1960,
+    "weeks_at_1": 3,
+    "consecutive": "TRUE",
+    "times_at_one": 1,
+    "year": 1965,
+    "preview_url": "https://p.scdn.co/mp3-preview/46aeed8650533caf77314fe7e46a015516bcaa1a?cid=ddba468408e2427090e0d79450f3d535"
+  };
+
+var chartsSong2 = {
+    "title": "LET'S DANCE",
+    "artist": "DAVID BOWIE",
+    "spotify_id": "0F0MA0ns8oXwGw66B2BSXm",
+    "danceability": 0.619,
+    "energy": 0.703,
+    "key": 10,
+    "loudness": -9.414,
+    "speechiness": 0.0525,
+    "acousticness": 0.00388,
+    "instrumentalness": 0.205,
+    "liveness": 0.163,
+    "valence": 0.709,
+    "tempo": 115.043,
+    "duration_ms": 457133,
+    "time_signature": 4,
+    "date": "09/04/1983",
+    "decade": 1980,
+    "weeks_at_1": 3,
+    "consecutive": "TRUE",
+    "times_at_one": 1,
+    "year": 1983,
+    "preview_url": "https://p.scdn.co/mp3-preview/1157e120445585f0155b9d1ca303029b8068d9a4?cid=ddba468408e2427090e0d79450f3d535"
+  };
+
+  var userSong1 = {
+    "title":"C'est La Vie",
+    "spotify_id":"2xt81cgiSP48aiv032Rwvx",
+    "artist":"Stereophonics",
+    "danceability":0.459,
+    "energy":0.925,
+    "key":10,
+    "loudness":-2.415,
+    "mode":1,
+    "speechiness":0.0442,
+    "acousticness":0.0471,
+    "instrumentalness":0,
+    "liveness":0.133,
+    "valence":0.827,
+    "tempo":148.054,
+    "type":"audio_features",
+    "id":"2xt81cgiSP48aiv032Rwvx",
+    "uri":"spotify:track:2xt81cgiSP48aiv032Rwvx",
+    "track_href":"https://api.spotify.com/v1/tracks/2xt81cgiSP48aiv032Rwvx",
+    "analysis_url":"https://api.spotify.com/v1/audio-analysis/2xt81cgiSP48aiv032Rwvx",
+    "duration_ms":220320,
+    "time_signature":4
+  };
+
+module.exports = {
+  chartsSong1,
+  chartsSong2,
+  userSong1
+}
