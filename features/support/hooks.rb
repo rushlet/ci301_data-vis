83bae0561f856d382acc80eb9c7faae0aed22395
Becky Rush <becky.rush14@yahoo.co.uk>
2018-04-03 18:43:11 +0100

    playlist button tests passing

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,14 +1,18 @@
 Before('@spotify-auth') do
   puts 'running hook'
   visit 'https://rushlet.github.io/ci301_data-vis/website/index.html'
-  sleep(2)
+  sleep(5)
   page.save_screenshot 'features/reports/screenshots/hook-start.png'
-  find("#spotify-log-in").click
-  url = current_url
-  url.include?("accounts.spotify.com/en/authorize?")
-  click_link('Log in to Spotify')
-  url = current_url
-  url.include?("accounts.spotify.com/en/login?continue=")
+  puts current_url
+  if current_url.include?("index.html")
+    find("#spotify-log-in").click
+    url = current_url
+    url.include?("accounts.spotify.com/en/authorize?")
+    click_link('Log in to Spotify')
+    url = current_url
+    url.include?("accounts.spotify.com/en/login?continue=")
+  end
+  page.save_screenshot 'features/reports/screenshots/hook-end.png'
 end
 
 Before('@personalisation-section') do
